3087b86a842fac4d2308b688c628d8426e6fe83f
hulyak <57311842+hulya95@users.noreply.github.com>
2020-04-01 12:48:49 -0700

    change active class

--- js/app.js
+++ js/app.js
@@ -17,38 +17,38 @@
 //  * Define Global Variables
 //  *
 // */
-// const sections = document.querySelectorAll('section');
-// const navList = document.querySelector('#navbar__list');
-// const nav = document.querySelector('.navbar__menu');
+const sections = document.querySelectorAll('section');
+const navList = document.querySelector('#navbar__list');
+const nav = document.querySelector('.navbar__menu');
 
-// /**
-//  * End Global Variables
-//  * Start Helper Functions
-//  *
-// */
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+*/
 
-// /**
-//  * End Helper Functions
-//  * Begin Main Functions
-//  *
-// */
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+*/
 
-// build the nav
-// function navigation() {
-// 	for (let item of sections) {
-// 		let section = document.createElement('li');
-// 		let anchor = document.createElement('a');
-// 		anchor.href = `#${item.id}`;
-// 		section.className = 'list-item';
-// 		section.dataset.nav = item.id;
-// 		anchor.innerText = item.dataset.nav;
-// 		// section.innerText = item.dataset.nav;
-// 		section.appendChild(anchor);
-// 		navList.appendChild(section);
-// 	}
-// }
+//build the nav
+function navigation() {
+	for (let item of sections) {
+		let section = document.createElement('li');
+		let anchor = document.createElement('a');
+		anchor.href = `#${item.id}`;
+		section.className = 'list-item';
+		section.dataset.nav = item.id;
+		anchor.innerText = item.dataset.nav;
+		// section.innerText = item.dataset.nav;
+		section.appendChild(anchor);
+		navList.appendChild(section);
+	}
+}
 
-// Add class 'active' to section when near top of viewport
+//Add class 'active' to section when near top of viewport
 // function activeClass() {
 // 	for (let activeElem of sections) {
 // 		let bounding = activeElem.getBoundingClientRect();
@@ -64,117 +64,123 @@
 // 		}
 // }
 
-// function active() {
-// 	window.addEventListener('scroll', (event) => {
-// 		for (let section of sections) {
-// 			const secTop = section.getBoundingClientRect().top;
-// 			const navBarUl = navList;
-// 			const firstList = navBarUl.firstElementChild;
-// 			const activeItemName = section.getAttribute('id');
-// 			const activeElementIndex = activeItemName.charAt(activeItemName.length - 1) - 1;
-// 			const activeElement = navBarUl.children[activeElementIndex];
-// 			if (isElementInViewport(section)) {
-// 				activeElement.classList.add('your-active-class');
-// 			} else {
-// 				activeElement.classList.remove('your-active-class');
-// 			}
+function active() {
+	window.addEventListener('scroll', (event) => {
+		for (let section of sections) {
+			const secTop = section.getBoundingClientRect().top;
+			const navBarUl = navList;
+			const firstList = navBarUl.firstElementChild;
+			const activeItemName = section.getAttribute('id');
+			const activeElementIndex = activeItemName.charAt(activeItemName.length - 1) - 1;
+			const activeElement = navBarUl.children[activeElementIndex];
+			if (isElementInViewport(section)) {
+				activeElement.classList.add('your-active-class');
+			} else {
+				activeElement.classList.remove('your-active-class');
+			}
 
-// 			function isElementInViewport(el) {
-// 				const rect = el.getBoundingClientRect();
+			// function isElementInViewport(el) {
+			// 	const rect = el.getBoundingClientRect();
 
-// 				return (
-// 					rect.top >= 50 &&
-// 					rect.left >= 0 &&
-// 					rect.bottom <=
-// 						(window.innerHeight || document.documentElement.clientHeight) /* or $(window).height() */ &&
-// 					rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
-// 				);
-// 			}
-// 		}
-// 	});
-// }
-// // fixed nav
-// let topOfNav = nav.offsetTop;
-// function fixNav() {
-// 	if (window.scrollY >= topOfNav) {
-// 		document.body.style.paddingTop = nav.offsetHeight + 'px';
-// 		document.body.classList.add('fixed-nav');
-// 	} else {
-// 		document.body.classList.remove('fixed-nav');
-// 		document.body.style.paddingTop = 0;
-// 	}
-// }
-// window.addEventListener('scroll', fixNav);
-// // Scroll to anchor ID using scrollTO event
+			// 	return (
+			// 		rect.top >= 0 &&
+			// 		rect.left >= 0 &&
+			// 		rect.bottom <=
+			// 			(window.innerHeight || document.documentElement.clientHeight) /* or $(window).height() */ &&
+			// 		rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+			// 	);
+			// }
+			function isElementInViewport(el) {
+				const rect = el.getBoundingClientRect();
+			  
+				return {
+				  top: rect.top + window.pageYOffset,
+				  left: rect.left + window.pageXOffset,
+				};
+			  }
+		}
+	});
+}
 
 
-// Build menu
-// navigation();
+// fixed nav
+let topOfNav = nav.offsetTop;
+function fixNav() {
+	if (window.scrollY >= topOfNav) {
+		document.body.style.paddingTop = nav.offsetHeight + 'px';
+		document.body.classList.add('fixed-nav');
+	} else {
+		document.body.classList.remove('fixed-nav');
+		document.body.style.paddingTop = 0;
+	}
+}
+window.addEventListener('scroll', fixNav);
+// Scroll to anchor ID using scrollTO event
 
-// // Scroll to section on link click
 
-// // Set sections as active
-// active();
-
-
-const navbar = document.querySelector('#navbar__list');
-const sections = document.querySelectorAll('section');
-const header = document.querySelector('.page__header');
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
- */
-
-// Set sections as active
-const setActive = target => {
-    document.querySelector('.your-active-class').classList.remove('your-active-class');
-    target.classList.add('your-active-class');
-};
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
- */
-
-// build the nav
-const navBuilder = () => {
-    for (let section of sections) {
-        let temp = `<li class="menu__link">${section.querySelector('h2').innerText}</li>`;
-        navbar.innerHTML += temp;
-    };
-    navbar.classList.toggle('hidden');
-};
-
-// Add class 'active' to section when near top of viewport
-document.addEventListener('scroll', e => {
-    for (const key in sections) {
-        if (window.pageYOffset >= (sections[key].offsetTop - 400)) {
-            setActive(sections[key]);
-        }
-    }
-});
-
-// Build menu 
-navBuilder();
-
-/**
- * End Main Functions
- * Begin Events
- * 
- */
+//Build menu
+navigation();
 
 // Scroll to section on link click
-navbar.addEventListener('click', e => {
-    let targetSect = document.querySelector(`[data-nav="${e.target.innerText}"]`);
-    window.scrollTo(0, targetSect.offsetTop);
-    navbar.classList.add('hidden');
-})
 
-// show or hide the navigation
-header.firstElementChild.addEventListener('click', () => {
-    navbar.classList.toggle('hidden');
-})
+// Set sections as active
+active();
+
+
+// const navbar = document.querySelector('#navbar__list');
+// const sections = document.querySelectorAll('section');
+// const header = document.querySelector('.page__header');
+
+
+// /**
+//  * End Global Variables
+//  * Start Helper Functions
+//  * 
+//  */
+
+// // Set sections as active
+// const setActive = target => {
+//     document.querySelector('.your-active-class').classList.remove('your-active-class');
+//     target.classList.add('your-active-class');
+// };
+
+// /**
+//  * End Helper Functions
+//  * Begin Main Functions
+//  * 
+//  */
+
+// // build the nav
+// const navBuilder = () => {
+//     for (let section of sections) {
+//         let temp = `<li class="menu__link">${section.querySelector('h2').innerText}</li>`;
+//         navbar.innerHTML += temp;
+//     };
+//     navbar.classList.toggle('hidden');
+// };
+
+// // Add class 'active' to section when near top of viewport
+// document.addEventListener('scroll', e => {
+//     for (const key in sections) {
+//         if (window.pageYOffset >= (sections[key].offsetTop - 400)) {
+//             setActive(sections[key]);
+//         }
+//     }
+// });
+
+// // Build menu 
+// navBuilder();
+
+
+
+// // Scroll to section on link click
+// navbar.addEventListener('click', e => {
+//     let targetSect = document.querySelector(`[data-nav="${e.target.innerText}"]`);
+//     window.scrollTo(0, targetSect.offsetTop);
+//     navbar.classList.add('hidden');
+// })
+
+// // show or hide the navigation
+// header.firstElementChild.addEventListener('click', () => {
+//     navbar.classList.toggle('hidden');
+// })
